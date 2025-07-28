69382294c4c25c07f04a66d02c7ad688f2574d20
陶浩南 <taoaaron5@gmail.com>
2025-07-28 21:08:10 +1000

    using shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-		extend: {
+  	extend: {
   		colors: {
   			brand: {
-  				100: '#EA6365',
+  				'100': '#EA6365',
   				DEFAULT: '#FA7275'
   			},
   			red: '#FF7474',
@@ -21,17 +21,17 @@ const config: Config = {
   			pink: '#EEA8FD',
   			orange: '#F9AB72',
   			light: {
-  				100: '#333F4E',
-  				200: '#A3B2C7',
-  				300: '#F2F5F9',
-  				400: '#F2F4F8'
+  				'100': '#333F4E',
+  				'200': '#A3B2C7',
+  				'300': '#F2F5F9',
+  				'400': '#F2F4F8'
   			},
   			dark: {
-  				100: '#04050C',
-  				200: '#131524'
+  				'100': '#04050C',
+  				'200': '#131524'
   			},
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
@@ -64,15 +64,17 @@ const config: Config = {
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
   			chart: {
-  				1: 'hsl(var(--chart-1))',
-  				2: 'hsl(var(--chart-2))',
-  				3: 'hsl(var(--chart-3))',
-  				4: 'hsl(var(--chart-4))',
-  				5: 'hsl(var(--chart-5))'
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
   			}
   		},
   		fontFamily: {
-  			poppins: ['var(--font-poppins)']
+  			poppins: [
+  				'var(--font-poppins)'
+  			]
   		},
   		boxShadow: {
   			'drop-1': '0px 10px 30px 0px rgba(66, 71, 97, 0.1)',
