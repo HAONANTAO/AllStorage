590c3a492283d826b38714dde1d084a108606a59
陶浩南 <taoaaron5@gmail.com>
2025-08-01 20:34:06 +1000

    update the user sign-up and helper functions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 "use server"
 
+// accountId	来自 Appwrite 的 userId，是注册后用户在 Auth 系统中的唯一标识符
+// OTP（邮箱登录）	安全、用户友好，不用密码，让用户点一下邮件链接即可完成登录
+
 import { ID, Query } from "node-appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { createAdminClient } from "../appwrite";
@@ -33,5 +36,25 @@ const sendEmailOTP=async(email:string)=>{
 const createAccount = async ({ fullName, email }: { fullName:string, email:string }) => {
   const existingUser = await getUserByEmail(email);
 
-  const accountId = await sendEmailOTP({email})
+  const accountId = await sendEmailOTP(email);
+
+
+  if(!accountId) throw new Error("Failed to send OTP");
+  if(!existingUser){
+    // create one
+    const {databases} = await createAdminClient();
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2uLl8zBoK0_iM5pNwJAC8hQ2f68YKtlgc7Q&s",
+        accountId,
+      }
+    )
+  }
+  return parseStringify({ accountId });
 };
+
