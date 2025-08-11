145f3232445155ac589a293e7b61ae87d0f2e89c
陶浩南 <taoaaron5@gmail.com>
2025-08-11 21:17:36 +1000

    the dropdown list for download URL

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import {
   Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from 'next/image';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,20 +12,76 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-const ActionsDropdown = (file:File) => {
+import { Models } from 'node-appwrite';
+import { actionsDropdownItems } from '@/constants';
+import Link from 'next/link';
+import { constructDownloadUrl } from '@/lib/utils';
+const ActionsDropdown = ({file}:{file:Models.Document}) => {
+  const [isModelOpen,setIsModelOpen] = useState(false);
+  const [isDropDownOpen,setIsDropDownOpen] = useState(false);
+  const [action,setAction] = useState<ActionType | null>(null);
   return (
-    <Dialog>
-      <DropdownMenu>
-  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
+    <Dialog open={isModelOpen} onOpenChange={setIsModelOpen}>
+      <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="open"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              className="shad-dropdown-item"
+              onClick={() =>
+                // 如果点击了 dialog给新的model弹窗
+                {
+                  setAction(actionItem);
+                  if (
+                    ["rename", "share", "delete", "details"].includes(
+                      actionItem.value,
+                    )
+                  ) {
+                    setIsModelOpen(true);
+                  }
+                }
+              }
+              key={actionItem.value}>
+                {/* 只有download点击直接可以下载 */}
+              {actionItem.value === "download" ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)}
+                  download={file.name}
+                  className="flex items-center gap-2">
+                  <Image
+                    src={actionItem.icon}
+                    alt={actionItem.value}
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={actionItem.icon}
+                    alt={actionItem.value}
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </div>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Dialog>
   );
 }
