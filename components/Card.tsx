5b3c3cd38bcfc4053453034e6c653212db1c3561
陶浩南 <taoaaron5@gmail.com>
2025-08-11 20:33:50 +1000

    the preview card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -22,6 +22,7 @@ const Card = ({file}:{file:Models.Document}) => {
       <div className="file-card-details">
         <p className="subtitle-2 line-clamp-1">{file.name}</p>
         <FormatedDateTime date={file.$createdAt} className="body-2 text-light-100"/>
+        <p className='caption line-clamp-1 text-light-200'>By:{file.owner.fullName}</p>
       </div>
       </Link>
      </>
--- components/FormatedDateTime.tsx
+++ components/FormatedDateTime.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { cn, formatDateTime } from '../lib/utils';
 
-const FormatedDateTime = ({date,className}:{date:string,className:string}) => {
+const FormatedDateTime = ({date,className}:{date:string,className?:string}) => {
   return <>
-  <p className={cn("body-1 text-light-200",className)}>formatDateTime(date)</p></>
+  <p className={cn("body-1 text-light-200",className)}>{formatDateTime(date)}</p>
+  
+  
+  </>
 }
 
 export default FormatedDateTime
