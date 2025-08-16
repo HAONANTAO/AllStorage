abc2a0d39fef57e50fe4113562c848217a37a3df
陶浩南 <taoaaron5@gmail.com>
2025-08-16 17:24:54 +1000

    renameFile creation

--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -1,12 +1,12 @@
-"use server"
+'use server';
 
-import { createAdminClient } from "../appwrite";
-import { InputFile } from "node-appwrite/file";
-import { appwriteConfig } from "../appwrite/config";
-import { ID, Query } from "node-appwrite";
-import { constructFileUrl, getFileType, parseStringify } from "../utils";
-import { revalidatePath } from "next/cache";
-import { getCurrentUser } from "./user.actions";
+import { createAdminClient } from '../appwrite';
+import { InputFile } from 'node-appwrite/file';
+import { appwriteConfig } from '../appwrite/config';
+import { ID, Query } from 'node-appwrite';
+import { constructFileUrl, getFileType, parseStringify } from '../utils';
+import { revalidatePath } from 'next/cache';
+import { getCurrentUser } from './user.actions';
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -22,9 +22,9 @@ export const uploadFile = async ({
   const { storage, databases } = await createAdminClient();
 
   try {
-    console.log("file size:", file.size);
+    console.log('file size:', file.size);
     if (!file || file.size === 0) {
-      throw new Error("file buffer is empty");
+      throw new Error('file buffer is empty');
     }
     // 如果 file 是浏览器的 File 对象，需要先转换：
     const buffer = Buffer.from(await file.arrayBuffer());
@@ -35,7 +35,7 @@ export const uploadFile = async ({
       ID.unique(),
       inputFile,
     );
-    console.log("test",bucketFile.$id);
+    console.log('test', bucketFile.$id);
     const fileDocument = {
       type: getFileType(bucketFile.name).type,
       name: bucketFile.name,
@@ -47,7 +47,7 @@ export const uploadFile = async ({
       users: [],
       bucketFileId: bucketFile.$id,
     };
-   
+
     const newFile = await databases
       .createDocument(
         appwriteConfig.databaseId,
@@ -57,48 +57,72 @@ export const uploadFile = async ({
       )
       .catch(async (error: unknown) => {
         await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
-        handleError(error, "Failed to create file document");
+        handleError(error, 'Failed to create file document');
       });
 
     revalidatePath(path);
     return parseStringify(newFile);
   } catch (error) {
-    handleError(error, "Failed to upload file");
+    handleError(error, 'Failed to upload file');
   }
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const createQueries= (currentUser:any)=>{
+const createQueries = (currentUser: any) => {
   const queries = [
     // 查找 owner 等于这个用户 ID 或者 users 包含这个用户的邮箱 的文档。
     Query.or([
-      Query.equal("owner", currentUser.$id),
-      Query.contains("users", currentUser.email),
+      Query.equal('owner', currentUser.$id),
+      Query.contains('users', currentUser.email),
     ]),
   ];
   // TODO:search sort limits...
   return queries;
-}
-export const getFiles = async()=>{
-  const {databases} = await createAdminClient();
-  try{
-    const currentUser =await getCurrentUser();
-    if(!currentUser){
-      throw new Error("user not found");
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+      throw new Error('user not found');
     }
     // search query creation
-    const queries = createQueries(currentUser)
-    
+    const queries = createQueries(currentUser);
+
     // get files
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
       queries,
-    )
+    );
     // console.log(files);
-    return parseStringify(files)
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, 'failed to getFiles from databases');
   }
-  catch(error){
-    handleError(error,"failed to getFiles from databases")
+};
+
+export const renameFile = async ({
+  fileId,
+  name,
+  extension,
+  path,
+}: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+  try {
+    const newName = `${name}.${extension}`;
+    const updatedFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId,
+      {
+        name: newName,
+      },
+    );
+    revalidatePath(path);
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, 'failed to rename file');
   }
-}
+};
