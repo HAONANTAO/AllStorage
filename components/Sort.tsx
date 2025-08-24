743c43806098dbcbcfddf6b4f3e9f6ea041ee105
陶浩南 <taoaaron5@gmail.com>
2025-08-24 15:41:12 +1000

    sort

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
-
+'use client';
+import React from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { usePathname, useRouter } from 'next/navigation';
+import { sortTypes } from '@/constants';
 const Sort = () => {
-  return <div>Sort Sort</div>;
-}
+  const router = useRouter();
+  const path = usePathname();
 
-export default Sort
+  // sort in getFiles
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem value={sort.value} key={sort.value}>
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Sort;
