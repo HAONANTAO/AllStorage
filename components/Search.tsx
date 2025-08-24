79dce573003f5516b1237c1e167b7506e6afda79
陶浩南 <taoaaron5@gmail.com>
2025-08-24 13:34:45 +1000

    the getFiles functions in search
    the getFiles functions in search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { Input } from './ui/input';
+import { useSearchParams } from 'next/navigation';
+import { getFiles } from '@/lib/actions/file.action';
+import { Models } from 'node-appwrite';
 
+// Whole page search function
 const Search = () => {
-  return <div>Search</div>;
-}
+  const [query, setQuery] = useState('');
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get('query') || '';
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ searchText: query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+    fetchFiles();
+  }, [query]);
 
-export default Search
+  useEffect(() => {
+    if (!searchQuery) {
+      // get all files
+      setQuery('');
+    }
+  }, []);
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          width={24}
+          height={24}
+          alt="search"
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => <li key={file.$id}>{file.name}</li>)
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
