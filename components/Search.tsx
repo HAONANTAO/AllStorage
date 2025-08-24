6635ceb3a68821d2fb1cda9d1b243ead4da08079
陶浩南 <taoaaron5@gmail.com>
2025-08-24 14:17:31 +1000

    the handleSearchClick

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,10 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Input } from './ui/input';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { getFiles } from '@/lib/actions/file.action';
 import { Models } from 'node-appwrite';
 import Thumbnail from './Thumbnail';
+import FormatedDateTime from './FormatedDateTime';
 
 // Whole page search function
 const Search = () => {
@@ -14,6 +15,7 @@ const Search = () => {
   const searchQuery = searchParams.get('query') || '';
   const [results, setResults] = useState<Models.Document[]>([]);
   const [open, setOpen] = useState(false);
+  const router = useRouter();
   useEffect(() => {
     const fetchFiles = async () => {
       const files = await getFiles({ searchText: query });
@@ -29,6 +31,17 @@ const Search = () => {
       setQuery('');
     }
   }, []);
+
+  const handleClickItem = (file: Models.Document) => {
+    // clear
+    setOpen(false);
+    setResults([]);
+    // and open this items in router
+    // TODO:没输入的时候点击可以跳转 但是不是单独显示
+    router.push(
+      `/${file.type === 'video' ? 'media' : file.type + 's'}?query=${query}`,
+    );
+  };
   return (
     <div className="search">
       <div className="search-input-wrapper">
@@ -51,15 +64,23 @@ const Search = () => {
                 <li
                   className="flex items-center justify-between"
                   key={file.$id}>
-                  <div className="flex cursor-pointer items-center gap-4">
+                  <div
+                    className="flex cursor-pointer items-center gap-4"
+                    onClick={() => handleClickItem(file)}>
                     <Thumbnail
                       type={file.type}
                       extension={file.extension}
                       url={file.url}
                       className="size-9 min-w-9 "
                     />
-                    {file.name}
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
                   </div>
+                  <FormatedDateTime
+                    className="caption line-clamp-1 text-light-200"
+                    date={file.$createdAt}
+                  />
                 </li>
               ))
             ) : (
