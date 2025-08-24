869902daac17e39a01b0c7d72320fbcc918df7fc
陶浩南 <taoaaron5@gmail.com>
2025-08-24 16:07:29 +1000

    fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,29 @@
-import Header from '@/components/Header'
-import MobileNavigation from '@/components/MobileNavigation'
-import SideBar from '@/components/SideBar'
-import { getCurrentUser } from '@/lib/actions/user.actions'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import { Toaster } from "@/components/ui/toaster";
- 
-const layout = async ({children}:{children:React.ReactNode}) => {
+import Header from '@/components/Header';
+import MobileNavigation from '@/components/MobileNavigation';
+import SideBar from '@/components/SideBar';
+import { getCurrentUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+import React from 'react';
+import { Toaster } from '@/components/ui/toaster';
+
+//
+export const dynamic = 'force-dynamic';
+const layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
-  if(! currentUser) return redirect("/sign-in")
+  if (!currentUser) return redirect('/sign-in');
   return (
     <main className="flex h-screen">
       <SideBar {...currentUser}></SideBar>
       <section className="flex h-full flex-1 flex-col ">
         <MobileNavigation {...currentUser}></MobileNavigation>
-        <Header ownerId={currentUser.$id} accountId={currentUser.accountId}></Header>
+        <Header
+          ownerId={currentUser.$id}
+          accountId={currentUser.accountId}></Header>
         <div className="main-content">{children}</div>
       </section>
       <Toaster />
     </main>
   );
-}
+};
 
-export default layout
+export default layout;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,132 +1,142 @@
-"use server"
+'use server';
 
 // accountId	来自 Appwrite 的 userId，是注册后用户在 Auth 系统中的唯一标识符
 // OTP（邮箱登录）	安全、用户友好，不用密码，让用户点一下邮件链接即可完成登录
 
-import { ID, Query } from "node-appwrite";
-import { appwriteConfig } from "../appwrite/config";
-import { createAdminClient, createSessionClient } from "../appwrite";
-import { parseStringify } from "../utils";
-import { cookies } from "next/headers";
-import { avatarPlaceholderUrl } from "@/constants";
-import { redirect } from "next/navigation";
+import { ID, Query } from 'node-appwrite';
+import { appwriteConfig } from '../appwrite/config';
+import { createAdminClient, createSessionClient } from '../appwrite';
+import { parseStringify } from '../utils';
+import { cookies } from 'next/headers';
+import { avatarPlaceholderUrl } from '@/constants';
+import { redirect } from 'next/navigation';
 
-const getUserByEmail=async (email:string)=>{
-  const {databases} = await createAdminClient();
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
   const result = await databases.listDocuments(
     appwriteConfig.databaseId,
     appwriteConfig.usersCollectionId,
-    [Query.equal("email", [email])],
+    [Query.equal('email', [email])],
   );
-  return result.total >0 ? result.documents[0] :null
-}
-const handleError = (error:unknown,message:string)=>{
-  console.log(error,message)
-  throw error
-}
+  return result.total > 0 ? result.documents[0] : null;
+};
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
 
 // appwrite给发一个email一次性登录的OTP
-export const sendEmailOTP=async(email:string)=>{
+export const sendEmailOTP = async (email: string) => {
   const { account } = await createAdminClient();
   try {
-    const session = await account.createEmailToken(ID.unique(),email)
-    return session.userId
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
   } catch (error) {
-    handleError(error,"Failed to send email OTP");
+    handleError(error, 'Failed to send email OTP');
   }
-
-}
+};
 
 // sign up function
-export const createAccount = async ({ fullName, email }: { fullName:string, email:string }) => {
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
   const existingUser = await getUserByEmail(email);
   if (existingUser) {
-    return { error: "User existed,redirect to sign-in" };
+    return { error: 'User existed,redirect to sign-in' };
   }
   const accountId = await sendEmailOTP(email);
 
-  if (!accountId) throw new Error("Failed to send OTP");
+  if (!accountId) throw new Error('Failed to send OTP');
 
-
-
-    // create one
-    const { databases } = await createAdminClient();
-    await databases.createDocument(
-      appwriteConfig.databaseId,
-      appwriteConfig.usersCollectionId,
-      ID.unique(),
-      {
-        fullName,
-        email,
-        avatar: avatarPlaceholderUrl,
-        accountId,
-      },
-    );
-    return parseStringify({ accountId });
-  }
-
+  // create one
+  const { databases } = await createAdminClient();
+  await databases.createDocument(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    ID.unique(),
+    {
+      fullName,
+      email,
+      avatar: avatarPlaceholderUrl,
+      accountId,
+    },
+  );
+  return parseStringify({ accountId });
+};
 
 // 用账号ID + OTP（一次性密码）尝试登录，看是否成功 → 成功就发放 session（登录凭证）
-export const verifySecret =async({accountId,password}:{accountId:string,password:string})=>{
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
   try {
     const { account } = await createAdminClient();
     const session = await account.createSession(accountId, password);
 
     //  将会话的 secret（凭证）存入浏览器的 cookie，配置了一些安全选项
-    (await cookies()).set("appwrite-session", session.secret, {
-      path: "/", // cookie 在根路径有效
+    (await cookies()).set('appwrite-session', session.secret, {
+      path: '/', // cookie 在根路径有效
       httpOnly: true, // 只能被服务器访问，前端 JS 无法访问，防止 XSS
-      sameSite: "strict", // 防止跨站请求伪造 CSRF
+      sameSite: 'strict', // 防止跨站请求伪造 CSRF
       secure: true, // 仅在 HTTPS 连接下传输 cookie
     });
     //  返回一个清理过的对象，只含 sessionId，避免传递复杂对象
     return parseStringify({ sessionId: session.$id });
   } catch (error) {
-    handleError(error,"Failed to verify OTP");
-    
+    handleError(error, 'Failed to verify OTP');
   }
- 
-}
+};
 // 提取session的user
-export const getCurrentUser = async()=>{
-  const {databases,account} = await createSessionClient();
-  const result = await account.get();
+export const getCurrentUser = async () => {
+  try {
+    const { databases, account } = await createSessionClient();
+    const result = await account.get();
 
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal("accountId",[result.$id])]
-  )
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal('accountId', [result.$id])],
+    );
 
-  if(user.total<=0) return null
-  return parseStringify(user.documents[0])
-}
+    if (user.total <= 0) return null;
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const signOutUser = async()=>{
-  const {account} = await createSessionClient();
+export const signOutUser = async () => {
+  const { account } = await createSessionClient();
   try {
     // Delete the current session
-    await account.deleteSession("current");
+    await account.deleteSession('current');
 
-    (await cookies()).delete("appwrite-session");
+    (await cookies()).delete('appwrite-session');
     // ✅ 只在成功时跳转
-    redirect("/sign-in");
+    redirect('/sign-in');
   } catch (error) {
-    handleError(error,"Failed to sign out user")
+    handleError(error, 'Failed to sign out user');
   }
-}
+};
 
-export const signInUser = async({email}:{email:string})=>{
-try {
-  const existedUser = await getUserByEmail(email)
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existedUser = await getUserByEmail(email);
 
-  // existed, send OTP
-  if(existedUser){
-    await sendEmailOTP(email)
-    return parseStringify({ accountId: existedUser.accountId });
+    // existed, send OTP
+    if (existedUser) {
+      await sendEmailOTP(email);
+      return parseStringify({ accountId: existedUser.accountId });
+    }
+    return parseStringify({ accountId: null, error: 'User not Found' });
+  } catch (error) {
+    handleError(error, 'Failed to sign in user');
   }
-  return parseStringify({ accountId: null, error: "User not Found" });
-} catch (error) {
-  handleError(error, "Failed to sign in user");
-}
-}
+};
